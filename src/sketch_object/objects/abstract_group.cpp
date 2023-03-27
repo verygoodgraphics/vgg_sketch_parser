30955d668db53013e199918b7f5ac44a9d550c94
szn <80252484@qq.com>
2023-03-27 15:47:43 +0800

    abstract_group: Relax the key requirements for json.

--- src/sketch_object/objects/abstract_group.cpp
+++ src/sketch_object/objects/abstract_group.cpp
@@ -1,4 +1,28 @@
-﻿#include "./abstract_group.h"
+﻿/*
+MIT License
+
+Copyright (c) 2023 Very Good Graphics
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "./abstract_group.h"
 #include "src/sketch_object/mask.h"
 
 t_child abstract_group::child_;
@@ -16,39 +40,30 @@ void abstract_group::change(const nlohmann::json &sketch, nlohmann::json &vgg)
    
     mask m_mask;
 
-    try 
-    {
-        vgg["class"] = "group";
-        vgg["childObjects"] = nlohmann::json::array();
+    vgg["class"] = "group";
+    vgg["childObjects"] = nlohmann::json::array();
 
-        auto &layers = sketch.at("layers");
-        for (auto &item : layers)
+    auto layers = get_json_item(sketch, "layers", "fail to get group layers");
+    for (auto &item : *layers)
+    {
+        string name;
+        get_json_value<string, string>(sketch, "_class", name, "fail to get obj _class");
+
+        auto it = child_.find(name);
+        if (it != child_.end())
         {
-            string name = item.at("_class").get<string>();
-            auto it = child_.find(name);
-            if (it != child_.end())
-            {
-                nlohmann::json out;
-                it->second->change(item, out);
-                
-                m_mask.deal_mask(*it->second, out);
+            nlohmann::json out;
+            it->second->change(item, out);
+            
+            m_mask.deal_mask(*it->second, out);
 
-                assert(!out.empty());
-                vgg["childObjects"].emplace_back(std::move(out));
-            }
-            else 
-            {
-                assert(name == "slice" || name == "MSImmutableHotspotLayer");
-            }
+            assert(!out.empty());
+            vgg["childObjects"].emplace_back(std::move(out));
         }
-    }
-    catch (sketch_exception &e)
-    {
-        throw e;
-    }
-    catch(...)
-    {
-        throw sketch_exception("fail to analyze group");
+        else 
+        {
+            assert(name == "slice" || name == "MSImmutableHotspotLayer");
+        }
     }
 
     /*
